e81e604c14e78a62b73c74fd7fa7a4d2f6696084
andre sekou <aszongo@gmail.com>
2014-01-14 22:44:15 +0000

    Starting a new file

--- DDL/schemas.h
+++ DDL/schemas.h
@@ -1,5 +1,52 @@
+ 
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-extern void connect();
+
+extern void connect() {
+	fprintf(stdout, "#! Usage: Connect -u {}\n");
+}
+
+extern void connect_db() {
+
+}
+
+extern void disconnect_db() {
+
+}
+
+extern void create_table(){
+
+}
+
+extern void insert() {
+
+}
+
+extern void update(){
+
+}
+
+extern void delete(){
+
+}
+
+extern void select(){
+
+}
+
+extern void show(){
+
+} // Parametres
+<<<<<<< HEAD
+
+extern void commit(){
+	
+=======
+
+extern void commit(){
+
+>>>>>>> 9dd7a24b20ea2a07298e29b80dc996d3947a450d
+}
