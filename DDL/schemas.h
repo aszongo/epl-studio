588d9cb12892e7b2b063da28d74b038da3d11676
andre sekou <aszongo@gmail.com>
2014-01-14 22:49:06 +0000

    Cleaning the HEADS
    not so easy to…

--- DDL/schemas.h
+++ DDL/schemas.h
@@ -10,7 +10,11 @@ extern void connect() {
 }
 
 extern void connect_db() {
+<<<<<<< HEAD
 
+=======
+	fprintf(stdout, "#! -- Usage: connect {db} [-u name] [-p password]\n");
+>>>>>>> 8778dc277802c7973ea005cb00e8d2aa71c454c2
 }
 
 extern void disconnect_db() {
@@ -41,6 +45,7 @@ extern void show(){
 
 } // Parametres
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 extern void commit(){
 	
@@ -49,4 +54,9 @@ extern void commit(){
 extern void commit(){
 
 >>>>>>> 9dd7a24b20ea2a07298e29b80dc996d3947a450d
+=======
+
+extern void commit(){
+
+>>>>>>> 8778dc277802c7973ea005cb00e8d2aa71c454c2
 }
