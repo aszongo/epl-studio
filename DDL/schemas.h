9dd7a24b20ea2a07298e29b80dc996d3947a450d
andre sekou <koupykon@gmail.com>
2014-01-14 22:27:36 +0000

    More prototypes
    We need 10+ functions to do the job, so this add is the beginning.

--- DDL/schemas.h
+++ DDL/schemas.h
@@ -1,5 +1,46 @@
 
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-extern void connect();
+
+extern void connect() {
+	fprintf(stdout, "#! Usage: Connect -u {}\n");
+}
+
+extern void connect_db() {
+
+}
+
+extern void disconnect_db() {
+
+}
+
+extern void create_table(){
+
+}
+
+extern void insert() {
+
+}
+
+extern void update(){
+
+}
+
+extern void delete(){
+
+}
+
+extern void select(){
+
+}
+
+extern void show(){
+
+} // Parametres
+
+extern void commit(){
+
+}
